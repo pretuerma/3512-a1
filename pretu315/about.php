94a1f67150590642ecd2b56b904cb80dcb7516e5
pretuerma <90541677+pretuerma@users.noreply.github.com>
2025-10-19 06:50:25 -0600

    Adding company.php and modifying about page

--- pretu315/about.php
+++ pretu315/about.php
@@ -16,7 +16,31 @@
 <hr>
 
 <h2>About This Project</h2>
-<p>This project was created for <strong>COMP 3512</strong> at Mount Royal University.</p>
+<p>
+  This project was created for <strong>COMP 3512</strong> at Mount Royal University.
+  <br></br>
+  It showcases a simple stock portfolio system where users can view company and portfolio information from a database.
+</p>
+
+<p>
+  The project was built using <strong>PHP</strong> for server-side scripting, <strong>CSS</strong> for styling,
+  and <strong>Visual Studio Code (VS Code)</strong> as the main development environment.
+  <br></br>
+  It runs on <strong>XAMPP</strong>
+  <br></br>
+  The code is version-controlled and hosted on <strong>GitHub</strong>.
+</p>
+
+<p>
+  <strong>Group Member:</strong> Pamela Retuerma
+</p>
+
+<p>
+  <strong>GitHub Repository:</strong>
+  <a href="https://github.com/pretuerma/3512-a1" target="_blank">
+    https://github.com/pretuerma/3512-a1
+  </a>
+</p>
 
 
 </body>
